00e33e5f6b0c2cafe11328ebde01629616787ab7
eis <6283064@gmail.com>
2025-02-09 19:48:45 +0700

    Простые числа [#505215 #531906]

--- src/test/java/ru/job4j/loop/PrimeNumber.java
+++ src/test/java/ru/job4j/loop/PrimeNumber.java
@@ -5,17 +5,11 @@ import static ru.job4j.loop.CheckPrimeNumber.*;
 public class PrimeNumber {
     public static int calc(int finish) {
         int countPrimeNumbers = 0;
-
-        if (finish <= 1) {
-            return countPrimeNumbers;
-        }
-
         for (int i = 2; i <= finish; i++) {
             if (check(i)) {
                 countPrimeNumbers++;
             }
         }
-
         return countPrimeNumbers;
     }
 }
