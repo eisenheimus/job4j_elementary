970489f0b5e33cf323985401d7bede73aefc5298
eis <6283064@gmail.com>
2025-02-10 22:15:14 +0700

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,16 @@ public class ArrayDefinition {
         System.out.println("Размер массива arr1 равен: " + arr1.length);
         System.out.println("Размер массива arr2 равен: " + arr2.length);
         System.out.println("Размер массива arr3 равен: " + arr3.length);
+
+        String[] names = new String[4];
+        names[0] = "Obi Wan Kenobi";
+        names[1] = "Anakin Skywalker";
+        names[2] = "Master Yoda";
+        names[3] = "Ahsoka Tano";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
