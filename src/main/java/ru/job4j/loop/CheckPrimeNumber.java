56d9852c0965c9a1bba76656a3ed64e3261e2a26
eis <6283064@gmail.com>
2025-02-10 15:32:57 +0700

    Простое число (оператор прерывания break) [#505214 #531904]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,14 +3,12 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int n) {
         boolean result = n > 1;
-
         for (int i = 2; i < n; i++) {
             if (n % i == 0) {
                 result = false;
                 break;
             }
         }
-
         return result;
     }
 }
