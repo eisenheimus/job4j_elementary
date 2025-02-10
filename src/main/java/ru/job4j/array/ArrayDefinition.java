5454646fdb4b20a085a12aa8f6a4dd4ec87384a5
eis <6283064@gmail.com>
2025-02-10 22:05:09 +0700

    Размер массива [#505221]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,9 @@ public class ArrayDefinition {
         short[] arr1 = new short[10];
         String[] arr2 = new String[100500];
         float[] arr3 = new float[40];
+
+        System.out.println("Размер массива arr1 равен: " + arr1.length);
+        System.out.println("Размер массива arr2 равен: " + arr2.length);
+        System.out.println("Размер массива arr3 равен: " + arr3.length);
     }
 }
