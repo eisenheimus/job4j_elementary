4d5b3c021ec3f5a6c9982a86ea716cbb351792ea
eis <6283064@gmail.com>
2025-02-10 15:30:39 +0700

    Протеиновая диета (цикл while) [#505213 #531899]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -2,17 +2,12 @@ package ru.job4j.loop;
 
 public class Fitness {
     public static int calc(int ivan, int nik) {
-        if (ivan == nik) {
-            return 1;
-        }
-
         int month = 0;
-        while (ivan < nik) {
+        while (ivan <= nik) {
             ivan *= 3;
             nik *= 2;
             month++;
         }
-
         return month;
     }
 }
