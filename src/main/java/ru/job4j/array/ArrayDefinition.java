75880560c768b0d28232d70cb1ba63465831f1b0
eis <6283064@gmail.com>
2025-02-10 22:01:16 +0700

    Объявление массива [#505220]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] arr1 = new short[10];
+        String[] arr2 = new String[100500];
+        float[] arr3 = new float[40];
+    }
+}
