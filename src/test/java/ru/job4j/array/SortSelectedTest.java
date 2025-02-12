86e1510d72e7f272c9d519d1a77f293a9239aabb
eis <6283064@gmail.com>
2025-02-13 00:27:09 +0700

    Сортировка выборкой [#505240]

---
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SortSelectedTest {
+    @Test
+    public void whenSortWhen34125Then12345() {
+        int[] data = new int[] {3, 4, 1, 2, 5};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 4, 5};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenSortWhen111Minus2ThenMinus2111() {
+        int[] data = new int[] {11, 1, -2};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {-2, 1, 11};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenSortWhen121040Then012410() {
+        int[] data = new int[] {1, 2, 10, 4, 0};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {0, 1, 2, 4, 10};
+        assertThat(result).containsExactly(expected);
+    }
+
+//    2, 5, 120, 7, 9, 12, 0, -2})));
+}
